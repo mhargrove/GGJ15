485afc52ba5249f262f6d9078098f67b17cce30d
Morgan Hargrove <mhargr5@lsu.edu>
2015-01-24 01:47:09 -0600

    clean up script code

--- DFT/Assets/Scripts/MoveCharacterBasic.cs
+++ DFT/Assets/Scripts/MoveCharacterBasic.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class MoveCharacterBasic : MonoBehaviour 
 {
-	
 	void Update () 
 	{
-
-				if (Input.GetKeyDown (KeyCode.LeftArrow)) 
-				{
-						transform.position = new Vector2 (transform.position.x - .32f, transform.position.y);
-				} 
-				else if (Input.GetKeyDown (KeyCode.RightArrow)) 
-				{
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			transform.position = new Vector2 (transform.position.x - .32f, transform.position.y);
+		} 
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
 			transform.position = new Vector2(transform.position.x + .32f, transform.position.y);
-				}
-		else if(Input.GetKeyDown(KeyCode.UpArrow))
-		{transform.position = new Vector2(transform.position.x, transform.position.y + .32f);
-		}else if(Input.GetKeyDown(KeyCode.DownArrow))
-		{transform.position = new Vector2(transform.position.x, transform.position.y - .32f);
 		}
-
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			transform.position = new Vector2(transform.position.x, transform.position.y + .32f);
 		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			transform.position = new Vector2(transform.position.x, transform.position.y - .32f);
+		}
+	}
 }
