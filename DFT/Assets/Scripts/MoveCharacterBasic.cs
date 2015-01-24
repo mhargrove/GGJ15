1723f5de7dde2a2fee3437fe62fcc841cbaf84a6
Hunter Crossett <hunter.crossett@gmail.com>
2015-01-24 00:43:26 -0600

    MOVEMENT
    works now

--- DFT/Assets/Scripts/MoveCharacterBasic.cs
+++ DFT/Assets/Scripts/MoveCharacterBasic.cs
@@ -1,70 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveCharacterBasic : MonoBehaviour {
-
-	public delegate void MoveGuyEvent();
-	/*public static event MoveGuyEvent OnTurnLeft; 
-	public static event MoveGuyEvent OnTurnRight; 
-	public static event MoveGuyEvent OnMoveLeft; 
-	public static event MoveGuyEvent OnMoveRight; 
-	public static event MoveGuyEvent OnStopMovining; */
-
-	public event MoveGuyEvent OnTurnLeft; 
-	public event MoveGuyEvent OnTurnRight; 
-	public event MoveGuyEvent OnMoveLeft; 
-	public event MoveGuyEvent OnMoveRight; 
-	public event MoveGuyEvent OnStopMovining;
-
-	
-	private KeyCode m_CurrentKey = KeyCode.None;
-	private bool m_Pressed = false;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
+public class MoveCharacterBasic : MonoBehaviour 
+{
+	void Update () 
+	{
 		if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
-			m_Pressed = true;
-			if(m_CurrentKey != KeyCode.LeftArrow)
-			{
-				m_CurrentKey = KeyCode.LeftArrow;
-				if(OnTurnLeft != null)
-					OnTurnLeft();
-			}
+			transform.position = new Vector2(transform.position.x - .32f, transform.position.y);
 		}
-		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		else if(Input.GetKeyDown(KeyCode.RightArrow))
 		{
-			m_Pressed = true;
-			if(m_CurrentKey != KeyCode.RightArrow)
-			{
-				m_CurrentKey = KeyCode.RightArrow;
-				if(OnTurnRight != null)
-					OnTurnRight();
-			}
+			transform.position = new Vector2(transform.position.x + .32f, transform.position.y);
 		}
-		else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+		else if(Input.GetKeyDown(KeyCode.UpArrow))
 		{
-			m_Pressed = false;
-			if(OnStopMovining != null)
-				OnStopMovining();
+			transform.position = new Vector2(transform.position.x, transform.position.y + .32f);
 		}
-		else if(m_Pressed && m_CurrentKey == KeyCode.LeftArrow)
+		else if(Input.GetKeyDown(KeyCode.DownArrow))
 		{
-			if(OnMoveLeft != null)
-				OnMoveLeft();
+			transform.position = new Vector2(transform.position.x, transform.position.y - .32f);
 		}
-		else if(m_Pressed && m_CurrentKey == KeyCode.RightArrow)
-		{
-			if(OnMoveRight != null)
-				OnMoveRight();
-		}
-
-	
 	}
 }
