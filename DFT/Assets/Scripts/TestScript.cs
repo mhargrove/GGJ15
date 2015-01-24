13e82cbb3af6645446cfb2b85a61f4eefc5d8e6e
Morgan Hargrove <mhargr5@lsu.edu>
2015-01-23 21:44:27 -0600

    update unity assets

---
+++ DFT/Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	[SerializeField] int m_TestInt = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
