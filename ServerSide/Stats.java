d94280c4fd1db716b205afb238e7096adb6fca29
Mitchell Mason <mmaso14@lsu.edu>
2015-01-24 18:54:15 -0600

    Working towards working items

--- ServerSide/Stats.java
+++ ServerSide/Stats.java
@@ -1,18 +1,24 @@
 public class Stats{
-	public final int MAXVALUE = 100; // max value for each stat
+	public static final int MAXVALUE = 100; // max value for each stat
 	public int health;
 	public int sleepy;
 	public int social;
-	public int food;
+	public int hungry;
 	public int romance;
 	public int study;
+	public int cash;
+
+	//considered each update.
+	public int timeDelta = 0;
+	public int moveFastFor = 0;
 
 	public Stats(){
 		health  = MAXVALUE;
 		sleepy  = 0;
 		social  = MAXVALUE / 2;
-		food    = MAXVALUE / 2;
+		hungry  = MAXVALUE / 2;
 		romance = MAXVALUE / 2;
 		study   = 0;
+		cash = 0;
 	}
 }
