24eb779d3ccfd51affc114dffe4aba53da724f5d
Everett Moser <foolsboy137@gmail.com>
2015-01-25 11:49:18 -0600

    IP address in intro

---
+++ DFT/Assets/Scripts/InputIP.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputIP : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Object.DontDestroyOnLoad (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- DFT/Assets/Scripts/LoadGame.cs
+++ DFT/Assets/Scripts/LoadGame.cs
@@ -1,27 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
 
 public class LoadGame : MonoBehaviour {
 
+	public GameObject ipAddressTextfield;
 
 	void Start ()
 	{
-		if (Application.loadedLevel == 0 || Application.loadedLevel == 4)
-			StartCoroutine("Load");
+		if (Application.loadedLevel == 0 || Application.loadedLevel == 4) 
+		{//StartCoroutine("Load");
+				}
 		else
 			StartCoroutine("LoadResults");
 	}
 
-	IEnumerator Load()
+	/*IEnumerator Load()
 	{
-		yield return new WaitForSeconds(8);
-		Application.LoadLevel(1);
+		//yield return new WaitForSeconds(8);
+		//Application.LoadLevel(1);
+		return 0;
 		}
-
+*/
 	IEnumerator LoadResults()
 	{
 		Object.DontDestroyOnLoad(GameObject.Find("results"));
 		yield return new WaitForSeconds(8);
 		Application.LoadLevel (4);
 		}
+
+	public void validate()
+	{
+		string teststring = ipAddressTextfield.transform.GetChild (0).transform.GetChild(2).GetComponent<Text>().text;
+		Debug.Log ("TEST" + teststring);
+		Application.LoadLevel(1);
+	}
 }
