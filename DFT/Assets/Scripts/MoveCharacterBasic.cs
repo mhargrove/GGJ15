be2de9783cfb3b7c4bf5534a948ec5953d51009f
Morgan Hargrove <mhargr5@lsu.edu>
2015-01-24 18:44:20 -0600

    working on UI

--- DFT/Assets/Scripts/MoveCharacterBasic.cs
+++ DFT/Assets/Scripts/MoveCharacterBasic.cs
@@ -4,6 +4,24 @@ using System.Collections;
 
 public class MoveCharacterBasic : MonoBehaviour 
 {
+	IEnumerator upFade() 
+	{
+		for (float f = 31f; f >= 0; f -= 1f) 
+		{
+			transform.position = new Vector2(transform.position.x, transform.position.y + .01f);
+			yield return new WaitForSeconds(.01f);
+		}
+	}
+	
+	IEnumerator downFade() 
+	{
+		for (float f = 31f; f >= 0; f -= 1f) 
+		{
+			transform.position = new Vector2(transform.position.x, transform.position.y - .01f);
+			yield return new WaitForSeconds(.01f);
+		}
+	}
+
 	IEnumerator leftFade() 
 	{
 		for (float f = 31f; f >= 0; f -= 1f) 
@@ -21,43 +39,24 @@ public class MoveCharacterBasic : MonoBehaviour
 			yield return new WaitForSeconds(.01f);
 		}
 	}
-
-	IEnumerator upFade() 
-	{
-		for (float f = 31f; f >= 0; f -= 1f) 
-		{
-			transform.position = new Vector2(transform.position.x, transform.position.y + .01f);
-			yield return new WaitForSeconds(.01f);
-		}
-	}
-
-	IEnumerator downFade() 
-	{
-		for (float f = 31f; f >= 0; f -= 1f) 
-		{
-			transform.position = new Vector2(transform.position.x, transform.position.y - .01f);
-			yield return new WaitForSeconds(.01f);
-		}
-	}
 	
 	void Update() 
 	{
-
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
-		{
-			StartCoroutine("leftFade");
-		} 
-		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
-		{
-			StartCoroutine("rightFade");
-		}
-		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			StartCoroutine("upFade");
 		}
 		else if (Input.GetKeyDown(KeyCode.DownArrow))
 		{
 			StartCoroutine("downFade");
+		}
+		else if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			StartCoroutine("leftFade");
+		} 
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			StartCoroutine("rightFade");
 		}
 	}
 }
